5b9dfb516f0aabdf57345ebd09fd7cf603e34649
Diana Johnson <dianaj4858@gmail.com>
2025-08-28 18:15:53 -0400

    Added description of file.

---
+++ youtube-ad-skipper/content.js
@@ -0,0 +1,6 @@
+/**
+ * @file This script runs on all YouTube pages and provides functionality to
+ * automatically skip video advertisements, mute ads, and retrieve current video information.
+ * @author dianajohnson49
+ * @version 1.2
+ */
