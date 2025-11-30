948a23db97f468e28ce8290955ef912bdcf4bd24
Diana Johnson <dianaj4858@gmail.com>
2025-11-30 13:24:00 -0500

    Added naive bayes classifier.

--- bayes-classifier-CS-6710/bayes_classifier.py
+++ bayes-classifier-CS-6710/bayes_classifier.py
@@ -43,7 +43,7 @@ def exact_bayes(X_train, y_train, X_test):
     for c in classes:
         # find all samples in train set that are class c
         Xc = X_train[y_train == c]
-        # get unique feature vectors/counts
+        # get unique feature vectors/counts for unique rows
         patterns, counts = np.unique(Xc, axis = 0, return_counts=True)
         model[c] = (patterns, counts, len(Xc))
     
@@ -79,5 +79,52 @@ def naive_bayes(X_train, y_train, X_test):
     X_train = X_train.astype(int)
     y_train = y_train.astype(int)
 
-    classes = np.unique(y_train)= 
+    classes = np.unique(y_train)
+    class_counts = {}
 
+    # get num of samples for each class
+    for c in classes:
+        count = np.sum(y_train == c)
+        class_counts[c] = count
+    
+    cond_prob = {}
+    for cls in classes:
+        Xc = X_train[y_train == cls]
+        cond_prob[cls] = {}
+
+        # loop through all features
+        for feature in range(X_train.shape[1]):
+            vals, counts = np.unique(Xc[:,feature], return_counts=True)
+
+            probs = {}
+            # get indeces and values for class c
+            for i, v in enumerate(vals):
+                count = counts[i]
+                probability = count / len(Xc)
+                probs[v] = probability
+            
+            cond_prob[cls][feature] = probs
+        
+    num_samples = len(y_train)
+    predictions = []
+
+    # for each test sample, assign prediction
+    for x in X_test:
+        max_prob = -1
+        max_class = None
+
+        for clas in classes:
+            prob = class_counts[clas] / num_samples
+            
+            for i, val in enumerate(x):
+                # for specific class c, and feature i, get the prob
+                # if val not found, use small value so it doesn't mult by 0
+                prob *= cond_prob[clas][i].get(val, 1e-9)
+            
+            if prob > max_prob:
+                max_prob = prob
+                max_class = clas
+
+        predictions.append(max_class)
+
+    return predictions
